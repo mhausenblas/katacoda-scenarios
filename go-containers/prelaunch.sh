e9962b7e51ce0abcda9c6b3dd06d3243832fbbb2
Michael Hausenblas <michael.hausenblas@gmail.com>
2018-06-22 11:42:12 +0100

    updates Go scenario

--- go-containers/prelaunch.sh
+++ go-containers/prelaunch.sh
@@ -1,3 +1,6 @@
+ssh root@host01 "export GOROOT=/usr/local/go"
+ssh root@host01 "export GOPATH=/root/go4ops"
+ssh root@host01 "export G4OHOME=/root/go4ops"
 ssh root@host01 "git clone https://github.com/mhausenblas/go4ops.git"
 ssh root@host01 "kubectl config set-context katacoda --cluster=kubernetes --namespace=test --user=kubernetes-admin && kubectl config use-context katacoda"
 sleep 10
