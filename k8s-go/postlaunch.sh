2b7451b2e831b600bd312ddecf1201605dedbb2d
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2018-07-27 13:15:53 +0200

    Move kubebuilder to /usr/local due to https://github.com/kubernetes-sigs/kubebuilder/issues/326

--- k8s-go/postlaunch.sh
+++ k8s-go/postlaunch.sh
@@ -1,4 +1,4 @@
 export GOROOT=/root/bin/go
 export GOPATH=/root/work
-export PATH=$GOPATH/bin:$GOROOT/bin:~/bin:~/bin/kubebuilder_1.0.0_linux_amd64/bin:$PATH
+export PATH=$GOPATH/bin:$GOROOT/bin:~/bin:/usr/local/kubebuilder/bin:$PATH
 export G4OHOME=/root/work/src/github.com/openshift-talks/k8s-go
