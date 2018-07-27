e5c0f243facbb8e47e8d0dd3b3efeed3057d9d79
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2018-07-27 16:21:43 +0200

    Move env vars into .profile

---
+++ k8s-go/assets/.profile
@@ -0,0 +1,5 @@
+. ~/.bashrc
+
+export GOPATH=/root/work
+export GOROOT=/root/bin/go
+export PATH=$GOPATH/bin:$GOROOT/bin:~/bin:/usr/local/kubebuilder/bin:$PATH
--- k8s-go/postlaunch.sh
+++ k8s-go/postlaunch.sh
@@ -1,4 +1,2 @@
-export GOROOT=/root/bin/go
 export GOPATH=/root/work
-export PATH=$GOPATH/bin:$GOROOT/bin:~/bin:/usr/local/kubebuilder/bin:$PATH
 export K8SGOHOME=/root/work/src/github.com/openshift-talks/k8s-go
