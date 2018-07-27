1b398ae19495e7bae3d41eea56fe6643c7901651
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2018-07-27 12:19:33 +0200

    Fix GOPATH in dep install command

--- k8s-go/prelaunch.sh
+++ k8s-go/prelaunch.sh
@@ -2,6 +2,6 @@ ssh root@host01 "mkdir -p /root/work/src/github.com/openshift-talks/k8s-go/"
 ssh root@host01 "git clone https://github.com/openshift-talks/k8s-go.git /root/work/src/github.com/openshift-talks/k8s-go/"
 ssh root@host01 "kubectl config set-context katacoda --cluster=kubernetes --namespace=test --user=kubernetes-admin && kubectl config use-context katacoda"
 ssh root@host01 "chmod +x ~/bin/kubebuilder"
-ssh root@host01 "mkdir -p $GOPATH/bin && curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh"
+ssh root@host01 "export GOPATH=/root/work; mkdir -p /root/work/bin && curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh"
 
 sleep 10
