7e3940595c2b1154ec9d42707a9df7a8b9576fd9
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2018-07-27 12:09:23 +0200

    Move PATH change into postlaunch

--- k8s-go/assets/.profile
+++
@@ -1,3 +0,0 @@
-. ~/.bashrc
-
-export PATH=~/bin:~/bin/kubebuilder_1.0.0_linux_amd64/bin:$PATH
--- k8s-go/prelaunch.sh
+++ k8s-go/prelaunch.sh
@@ -1,4 +1,6 @@
 ssh root@host01 "mkdir -p /root/work/src/github.com/openshift-talks/k8s-go/"
 ssh root@host01 "git clone https://github.com/openshift-talks/k8s-go.git /root/work/src/github.com/openshift-talks/k8s-go/"
 ssh root@host01 "kubectl config set-context katacoda --cluster=kubernetes --namespace=test --user=kubernetes-admin && kubectl config use-context katacoda"
+ssh root@host01 "chmod +x ~/bin/kubebuilder"
+
 sleep 10
