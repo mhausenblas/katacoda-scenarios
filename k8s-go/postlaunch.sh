b8cf8be4601796d62c79b8383e2e519c5e4963d0
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2018-07-27 12:24:35 +0200

    Install dep wrapper

--- k8s-go/postlaunch.sh
+++ k8s-go/postlaunch.sh
@@ -1,4 +1,4 @@
-export PATH=~/bin:~/bin/kubebuilder_1.0.0_linux_amd64/bin:$PATH
 export GOROOT=/usr/local/go
 export GOPATH=/root/work
+export PATH=$GOPATH/bin:~/bin:~/bin/kubebuilder_1.0.0_linux_amd64/bin:$PATH
 export G4OHOME=/root/work/src/github.com/openshift-talks/k8s-go
