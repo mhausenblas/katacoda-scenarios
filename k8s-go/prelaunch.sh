543c22cea120666faae233aa072a338179fc4688
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2018-07-27 12:57:14 +0200

    Install go 1.10

--- k8s-go/prelaunch.sh
+++ k8s-go/prelaunch.sh
@@ -1,3 +1,4 @@
+ssh root@host01 "curl -sf https://dl.google.com/go/go1.10.3.linux-amd64.tar.gz | tar -xz -C /root/bin"
 ssh root@host01 "mkdir -p /root/work/src/github.com/openshift-talks/k8s-go/"
 ssh root@host01 "git clone https://github.com/openshift-talks/k8s-go.git /root/work/src/github.com/openshift-talks/k8s-go/"
 ssh root@host01 "kubectl config set-context katacoda --cluster=kubernetes --namespace=test --user=kubernetes-admin && kubectl config use-context katacoda"
