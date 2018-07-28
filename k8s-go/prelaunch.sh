7da98582e552a9bbb48245d315f235668ef843f6
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2018-07-28 11:16:06 +0200

    Add operator-sdk

--- k8s-go/prelaunch.sh
+++ k8s-go/prelaunch.sh
@@ -2,6 +2,6 @@ ssh root@host01 "curl -sf https://dl.google.com/go/go1.10.3.linux-amd64.tar.gz |
 ssh root@host01 "mkdir -p /root/work/src/github.com/openshift-talks/k8s-go/"
 ssh root@host01 "git clone https://github.com/openshift-talks/k8s-go.git /root/work/src/github.com/openshift-talks/k8s-go/"
 ssh root@host01 "kubectl config set-context katacoda --cluster=kubernetes --namespace=test --user=kubernetes-admin && kubectl config use-context katacoda"
-ssh root@host01 "chmod +x ~/bin/kubebuilder ~/bin/dep"
+ssh root@host01 "chmod +x ~/bin/kubebuilder ~/bin/dep ~/bin/operator-sdk"
 
 sleep 10
