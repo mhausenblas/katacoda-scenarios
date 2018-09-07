4066946570522118baa1caa6e28dedfcb6cf62c5
Michael Hausenblas <michael.hausenblas@gmail.com>
2018-09-07 19:29:09 +0100

    updates Linkerd2

--- linkerd2/assets/expose-dashboard.sh
+++ linkerd2/assets/expose-dashboard.sh
@@ -13,7 +13,7 @@ if [ $? -ne 0 ]; then
     --pidfile /tmp/dashboard.pid \
     -S \
     --startas /bin/bash \
-    -- -c "exec $(pwd)/.linkerd2/bin/linkerd dashboard --port 8080 --url &> /root/dashboard.log"
+    -- -c "exec linkerd dashboard --port 8080 &> /root/dashboard.log"
 fi
 
 start-stop-daemon --status --pidfile /tmp/ngrok-dashboard.pid
