275024e972a9ca1980a8a6a824b513e590506b9c
Michael Hausenblas <michael.hausenblas@gmail.com>
2018-09-07 19:16:01 +0100

    updates Linkerd2

--- linkerd2/assets/expose-dashboard.sh
+++ linkerd2/assets/expose-dashboard.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-kubectl apply -f loadbalancer/
-
-kubectl patch -n=linkerd svc web --patch '{ "spec": {"type": "LoadBalancer" } }'
+curl https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip 2> /dev/null \
+  | zcat > /usr/local/bin/ngrok \
+  && chmod a+x /usr/local/bin/ngrok
 
 start-stop-daemon --status --pidfile /tmp/dashboard.pid
 
@@ -16,12 +16,23 @@ if [ $? -ne 0 ]; then
     -- -c "exec $(pwd)/.linkerd2/bin/linkerd dashboard --port 8080 --url &> /root/dashboard.log"
 fi
 
+start-stop-daemon --status --pidfile /tmp/ngrok-dashboard.pid
+
+if [ $? -ne 0 ]; then
+  start-stop-daemon \
+    --background \
+    --make-pidfile \
+    --pidfile /tmp/ngrok-dashboard.pid \
+    -S \
+    --startas /bin/bash \
+    -- -c "exec /usr/local/bin/ngrok http --log stdout --log-level debug --host-header rewrite 8080 &> /root/ngrok.log"
+  sleep 3
+fi
+
 printf "The dashboard is available at:\n\n"
 
-sleep 30
-
-# note: this is an IP in the private range, need to make it publicly available:
-
-echo http://$(kubectl get -n=linkerd svc/web -o go-template='{{(index .status.loadBalancer.ingress 0).ip}}'):8084/
+echo $(cat ngrok.log \
+  | sed -n 's/.* URL:\([^ ]*\) .*/\1/p' \
+  | head -n1)"/api/v1/namespaces/linkerd/services/web:http/proxy/"
 
 printf "\ncut and paste this URL into your browser.\n"
