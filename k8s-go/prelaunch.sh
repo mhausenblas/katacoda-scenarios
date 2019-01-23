4f90f71968c4909241e94b575af63ae1f0eda1fc
Michael Hausenblas <michael.hausenblas@gmail.com>
2019-01-23 10:38:10 +0000

    removes context set in pre-launch

--- k8s-go/prelaunch.sh
+++ k8s-go/prelaunch.sh
@@ -2,7 +2,6 @@ ssh root@host01 "chmod +x ~/bin/kubebuilder ~/bin/dep ~/bin/operator-sdk"
 ssh root@host01 "curl -sf https://dl.google.com/go/go1.10.3.linux-amd64.tar.gz | tar -xz -C /root/bin"
 ssh root@host01 "mkdir -p /root/work/src/github.com/openshift-talks/k8s-go/"
 ssh root@host01 "git clone https://github.com/openshift-talks/k8s-go.git /root/work/src/github.com/openshift-talks/k8s-go/"
-ssh root@host01 "kubectl config set-context katacoda --cluster=kubernetes --namespace=default --user=kubernetes-admin && kubectl config use-context katacoda"
 ssh root@host01 "apt -y install make jq nano emacs"
 
 sleep 10
