b9a4f14808037a5fce6e883aaa899cb9fe2d9084
Michael Hausenblas <michael.hausenblas@gmail.com>
2019-01-28 19:00:22 +0100

    fixes Git setup in Kubernetes Go

--- k8s-go/postlaunch.sh
+++ k8s-go/postlaunch.sh
@@ -3,5 +3,7 @@ export GOROOT=/root/bin/go
 export PATH=$GOPATH/bin:$GOROOT/bin:~/bin:/usr/local/kubebuilder/bin:$PATH
 export K8SGOHOME=/root/work/src/github.com/openshift-talks/k8s-go
 export PS1='${PWD#"${PWD%/*/*}/"} \$ '
+git config --global user.email "someone@example.com"
+git config --global user.name "Kubernaut"
 launch.sh
 kubectl config set-context katacoda --cluster=kubernetes --namespace=default --user=kubernetes-admin
--- k8s-go/prelaunch.sh
+++ k8s-go/prelaunch.sh
@@ -3,5 +3,3 @@ ssh root@host01 "curl -sf https://dl.google.com/go/go1.10.3.linux-amd64.tar.gz |
 ssh root@host01 "mkdir -p /root/work/src/github.com/openshift-talks/k8s-go/"
 ssh root@host01 "git clone https://github.com/openshift-talks/k8s-go.git /root/work/src/github.com/openshift-talks/k8s-go/"
 ssh root@host01 "apt -y install make jq nano emacs"
-
-sleep 10
